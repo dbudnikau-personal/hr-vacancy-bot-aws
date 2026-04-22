988675786dfee4705f23a12a303e3e6b6b465ee7
dbudnikau <247458266+dbudnikau-personal@users.noreply.github.com>
2026-04-22 19:15:00 +0400

    Remove .aws-sam build artifacts from git

--- .aws-sam/build/BotHandlerFunction/db/migration/V3__create_hh_areas.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE hh_areas (
-    id          VARCHAR(20) PRIMARY KEY,
-    name        VARCHAR(255) NOT NULL,
-    parent_id   VARCHAR(20),
-    name_lower  VARCHAR(255) NOT NULL
-);
-
-CREATE INDEX idx_hh_areas_name_lower ON hh_areas(name_lower);
